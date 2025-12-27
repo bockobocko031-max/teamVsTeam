285d6f9270d7f971feb5aaac9ea6ff83f4e80361
yambobkata123 <yambobiyam@gmail.com>
2025-12-27 16:21:36 +0200

    Initial project structure

---
+++ src/main/java/me/bobiyam/teamvsteam/TeamVsTeam.java
@@ -0,0 +1,17 @@
+package me.bobiyam.teamvsteam;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TeamVsTeam extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
