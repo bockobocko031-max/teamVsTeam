2bdedaa3c348c67fc34c6c64edf9238f02529f08
yambobkata123 <yambobiyam@gmail.com>
2025-12-28 12:52:09 +0200

    Edited TeamListener

--- src/main/java/me/bobiyam/teamvsteam/TeamListener.java
+++ src/main/java/me/bobiyam/teamvsteam/TeamListener.java
@@ -1,14 +1,9 @@
 package me.bobiyam.teamvsteam;
 
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
-
-import java.util.List;
-import java.util.Map;
 
 public class TeamListener implements Listener {
 
